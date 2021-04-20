c32d9bb920b7ac73d793d8660031396f356e1ebc
Aureliano Claudio <aurelianoclaudio29@gmail.com>
2021-04-20 15:17:28 -0300

    modificação nas variáveis no JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function gerarCor() {
-    let randomColor = '#' + Math.floor(Math.random() * 16777215) .toString(16)
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
     document.body.style.backgroundColor = randomColor
 
     const colorText = document.getElementById('colorText')

f84f9ddc048666d964058437e7c67df7fbf4da3f
Aureliano Claudio <aurelianoclaudio29@gmail.com>
2021-04-20 15:13:13 -0300

    correção de problemas no CSS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ function gerarCor() {
     let randomColor = '#' + Math.floor(Math.random() * 16777215) .toString(16)
     document.body.style.backgroundColor = randomColor
 
-    var colorText = document.getElementById('colorText')
+    const colorText = document.getElementById('colorText')
     colorText.innerText = randomColor
 }
